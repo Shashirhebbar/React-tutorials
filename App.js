a982ca6a103a22579de4914bbc76086402a409c9
Shashirhebbar <shashirhebbar@gmail.com>
2023-06-22 21:30:13 +0530

    Changes in App.js

--- App.js
+++ App.js
@@ -3,11 +3,19 @@ function Heading() {
       <h1>This is an h1 heading</h1>
     )
   }
+
+  function Heading2() {
+    return (
+      <h2>This is an h2 heading</h2>
+    )
+  }
+
   function App() {
     return ( 
       <div className="App"> 
         This is the starting code for "Your first component" ungraded lab 
-        <Heading /> 
+        <Heading />
+        <Heading2 /> 
       </div> 
     ); 
   } 
